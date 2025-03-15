505bc752db9ce3d02490191fef10a0974670d985
wwwxxx0402 <40693577+wwwxxx0402@users.noreply.github.com>
2025-03-15 17:07:30 +0800

    Update diy-part2.sh
    修改网络配置信息
    1. 添加wan6配置
    2. 添加 删除br-lan设备
    3. 修改ifname -> device

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,18 +21,23 @@
 
 # 网络配置信息，将从 zzz-default-settings 文件的第2行开始添加
 sed -i "2i # network config" ./package/lean/default-settings/files/zzz-default-settings
-# LAN
-sed -i "3i uci set network.lan.ipaddr='192.168.255.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认 IP 地址
-sed -i "4i uci set network.lan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
-sed -i "5i uci set network.lan.ifname='eth1'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口
-sed -i "6i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
 # WAN
-sed -i "7i uci set network.wan.ipaddr='192.168.0.2'" ./package/lean/default-settings/files/zzz-default-settings
-sed -i "8i uci set network.wan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
-sed -i "9i uci set network.wan.ifname='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口
-sed -i "10i uci set network.wan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
-sed -i "11i uci set network.wan.gateway='192.168.0.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址
-sed -i "12i uci set network.wan.dns='192.168.0.253'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
-sed -i "13i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
-#加入编译者信息
-sed -i "s/LEDE /WWW build $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.wan.ipaddr='192.168.0.2'" ./package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.wan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
+sed -i "5i uci set network.wan.device='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口
+sed -i "6i uci set network.wan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
+sed -i "7i uci set network.wan.gateway='192.168.0.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址
+sed -i "8i uci set network.wan.dns='192.168.0.253'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+# WAN6
+sed -i "9i uci set network.wan6.device='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口
+sed -i "10i uci set network.wan6.proto='dhcp'" ./package/lean/default-settings/files/zzz-default-settings  # dhcp
+
+# LAN
+sed -i "11i uci set network.lan.ipaddr='192.168.255.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认 IP 地址
+sed -i "12i uci set network.lan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
+sed -i "13i uci set network.lan.device='eth1'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口
+sed -i "14i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
+# br-lan
+sed -i "15i uci delete network.@device[0]" ./package/lean/default-settings/files/zzz-default-settings    # 删除br-lan设备
+
+sed -i "16i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
