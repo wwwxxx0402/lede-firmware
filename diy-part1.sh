138f19a78652c1f448db93208268b02a0449762e
wwwxxx0402 <40693577+wwwxxx0402@users.noreply.github.com>
2026-02-02 11:14:19 +0800

    更改passwall仓库地址到迁移后的位置

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
-sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' feeds.conf.default
+sed -i '2i src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git' feeds.conf.default
